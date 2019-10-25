5328911ae15e3cf4e4ad6d05f12a2d8eec9cd6d1
Povrty <iakwan@s.sfusd.edu>
2019-10-25 04:58:06 +0000

    Finished click to answer.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,34 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+$("#rock").hide();
+$("#paper").hide();
+$("#scissors").hide();
+$(".cal").hide();
+
 $(".rock").click(function() {
-    $("#userChoice").text();
+    $("#rock").show();
+    $("#paper").hide();
+    $("#scissors").hide();
+    $(".cal").show();
+    $(".cal").delay(5000).hide();
+});
+$(".paper").click(function() {
+    $("#paper").show();
+    $("#rock").hide();
+    $("#scissors").hide();
+    $(".cal").show();
+    $(".cal").delay(5000).hide();
+});
+$(".scissors").click(function() {
+    $("#scissors").show();
+    $("#rock").hide();
+    $("#paper").hide();
+    $(".cal").fadeIn(2500);
+    $(".cal").delay(2500).fadeOut();
 });
 
+var chance = Math.random();
 
 // DOCUMENT READY FUNCTION BELOW
 
