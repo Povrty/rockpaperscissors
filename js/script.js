ad1b9f7cf6ad666ec23c88afd74b13f43ed04a70
Povrty <iakwan@s.sfusd.edu>
2019-11-05 02:11:19 +0000

    Almost done with Winner output.

--- js/script.js
+++ js/script.js
@@ -6,27 +6,28 @@ $("#paperC").hide();
 $("#scissorsC").hide();
 $("h4").hide();
 
-$(".rock").click(function() {
+function Rock() {
     $("#rock").show();
     $("#paper").hide();
     $("#scissors").hide();
     $("h4").show();
-    Computer();
-});
-$(".paper").click(function() {
+    return "rockU";
+}
+
+function Paper() {
     $("#paper").show();
     $("#rock").hide();
     $("#scissors").hide();
     $("h4").show();
-    Computer();
-});
-$(".scissors").click(function() {
+    return "paperU";
+}
+function Scissors() {
     $("#scissors").show();
     $("#rock").hide();
     $("#paper").hide();
     $("h4").show();
-    Computer();
-});
+    return "scissorsU";
+}
 
 function Computer() {
     var chance = Math.random();
@@ -34,14 +35,48 @@ function Computer() {
         $("#rockC").show();
         $("#paperC").hide();
         $("#scissorsC").hide();
+        return "rockC";
     } else if (chance < 0.67) {
         $("#rockC").hide();
         $("#paperC").show();
         $("#scissorsC").hide();
+        return "paperC";
     } else {
         $("#rockC").hide();
         $("#paperC").hide();
         $("#scissorsC").show();
+        return "scissorsC";
     }
 }
+
+function Winner {
+    if (answerUr === "rockU" && answerCr === "rockC" || answerUp === "paperU" && answerCp === "paperC" || answerUs === "scissorsU" && answerCs === "scissorsC") {
+        alert();
+    }
+}
+
+$(".rock").click(function() {
+    Rock();
+    Computer();
+    var answerUr = Rock();
+    console.log(answerUr);
+    var answerCr = Computer();
+    console.log(answerCr);
+});
+$(".paper").click(function() {
+    Paper();
+    Computer();
+    var answerUp = Paper();
+    console.log(answerUp);
+    var answerCp = Computer();
+    console.log(answerCp);
+});
+$(".scissors").click(function() {
+    Scissors();
+    Computer();
+    var answerUs = Scissors();
+    console.log(answerUs);
+    var answerCs = Computer();
+    console.log(answerCs);
+});
 
